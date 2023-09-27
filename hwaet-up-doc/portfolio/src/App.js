f5edd179be56c2f0090145370155908803f80109
BenHwaet <benmdesroches@gmail.com>
2023-09-26 22:28:36 -0300

    restarted project, changed file organization and started components - using React Bootstrap

---
+++ hwaet-up-doc/portfolio/src/App.js
@@ -0,0 +1,15 @@
+
+import React from 'react';
+import './App.css';
+import Navbar from './components/Navbar';
+import AboutMe from './components/AboutMe';
+
+function Portfolio() {
+  return (
+    <div>
+      
+    </div>
+  );
+}
+
+export default App;
